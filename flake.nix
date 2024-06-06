71a5526e9332aa4335facde28f7029dcff6f4cab
NotLebedev <artem-itf@yandex.ru>
2024-06-06 16:47:06 +0300

    Update nix crane

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
 
         inherit (pkgs) lib;
 
-        craneLib = crane.lib.${system};
+        craneLib = (crane.mkLib nixpkgs.legacyPackages.${system});
         src = craneLib.cleanCargoSource (craneLib.path ./.);
 
         llvm = pkgs.llvmPackages_17;
